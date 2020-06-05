f52f0a306f1369ddb19aaf85ea2e8a69fe0a99c7
Aleksi Kangas <aokang@utu.fi>
2020-06-05 15:25:38 +0300

    Part 2.1-2.5

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Course = ({ course }) => {
+    return(
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+const Header = ({ name }) => {
+    return (
+        <h1>{name}</h1>
+    )
+}
+
+const Content = ({ parts }) => {
+    return (
+        <div>
+            {parts.map((part) =>
+                <Part key={part.name} part={part}/>
+            )}
+        </div>
+    )
+}
+
+const Part = ({ part }) => {
+    return (
+        <p key={part.name}>{part.name} {part.exercises}</p>
+    )
+}
+
+const Total = ({ parts }) => {
+    const sum = parts.reduce((sum, value) => sum + value.exercises, 0)
+    return (
+        <p><b>Number of exercises {sum}</b></p>
+    )
+}
+
+export default Course
