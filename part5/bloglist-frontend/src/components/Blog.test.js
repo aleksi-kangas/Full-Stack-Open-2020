a13d9e739f75463dcc142bd6a1d80307bf3e8ba6
Aleksi Kangas <aokang@utu.fi>
2020-07-17 23:06:32 +0300

    Part 5.13-5.15

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -4,72 +4,72 @@ import Blog from './Blog'
 
 
 describe('<Blog />', () => {
-    let component
-    const likeBlog = jest.fn()
-    const deleteBlog = jest.fn()
-    const blog = {
-        title: 'Blog Title',
-        author: 'Blog Author',
-        url: 'Blog Url',
-        user: {
-            username: 'Username',
-            name: 'User Name',
-            id: 'User Id'
-        },
-        likes: 1
-    }
+  let component
+  const likeBlog = jest.fn()
+  const deleteBlog = jest.fn()
+  const blog = {
+    title: 'Blog Title',
+    author: 'Blog Author',
+    url: 'Blog Url',
+    user: {
+      username: 'Username',
+      name: 'User Name',
+      id: 'User Id'
+    },
+    likes: 1
+  }
 
-    beforeEach(() => {
-        const user = {}
+  beforeEach(() => {
+    const user = {}
 
-        component = render(
-            <Blog blog={blog} likeBlog={likeBlog} deleteBlog={deleteBlog} user={user} />
-        )
+    component = render(
+      <Blog blog={blog} likeBlog={likeBlog} deleteBlog={deleteBlog} user={user} />
+    )
+  })
+
+  describe('Visibility of a blog\'s properties', () => {
+    test('by default only title and author are visible', () => {
+      const div = component.container.querySelector('.blog')
+      expect(div).toHaveTextContent(blog.title)
+      expect(div).toHaveTextContent(blog.author)
+
+      expect(div).not.toHaveTextContent(blog.url)
+      expect(div).not.toHaveTextContent(blog.likes)
+
+
     })
+    test('after clicking "View"-button, all blog\'s properties are displayed', () => {
+      const button = component.getByText('View')
+      fireEvent.click(button)
 
-    describe('Visibility of a blog\'s properties', () => {
-        test('by default only title and author are visible', () => {
-            const div = component.container.querySelector('.blog')
-            expect(div).toHaveTextContent(blog.title)
-            expect(div).toHaveTextContent(blog.author)
-
-            expect(div).not.toHaveTextContent(blog.url)
-            expect(div).not.toHaveTextContent(blog.likes)
-
-
-        })
-        test('after clicking "View"-button, all blog\'s properties are displayed', () => {
-            const button = component.getByText('View')
-            fireEvent.click(button)
-
-            const div = component.container.querySelector('.blog')
-            expect(div).toHaveTextContent(blog.title)
-            expect(div).toHaveTextContent(blog.author)
-            expect(div).toHaveTextContent(blog.url)
-            expect(div).toHaveTextContent(blog.likes)
-        })
-        test('after clicking the "View/Hide"-button twice, only title and author are visible', () => {
-            const button = component.getByText('View')
-            fireEvent.click(button)
-            fireEvent.click(button)
-
-            const div = component.container.querySelector('.blog')
-            expect(div).toHaveTextContent(blog.title)
-            expect(div).toHaveTextContent(blog.author)
-            expect(div).not.toHaveTextContent(blog.url)
-            expect(div).not.toHaveTextContent(blog.likes)
-        })
+      const div = component.container.querySelector('.blog')
+      expect(div).toHaveTextContent(blog.title)
+      expect(div).toHaveTextContent(blog.author)
+      expect(div).toHaveTextContent(blog.url)
+      expect(div).toHaveTextContent(blog.likes)
     })
-    describe('Like functionality', () => {
-        test('Like button clicked twice, event handler responsible for likes is called twice', () => {
-            const viewButton = component.getByText('View')
-            fireEvent.click(viewButton)
+    test('after clicking the "View/Hide"-button twice, only title and author are visible', () => {
+      const button = component.getByText('View')
+      fireEvent.click(button)
+      fireEvent.click(button)
 
-            const likeButton = component.getByText('Like')
-            fireEvent.click(likeButton)
-            fireEvent.click(likeButton)
-
-            expect(likeBlog.mock.calls.length).toBe(2)
-        })
+      const div = component.container.querySelector('.blog')
+      expect(div).toHaveTextContent(blog.title)
+      expect(div).toHaveTextContent(blog.author)
+      expect(div).not.toHaveTextContent(blog.url)
+      expect(div).not.toHaveTextContent(blog.likes)
     })
+  })
+  describe('Like functionality', () => {
+    test('Like button clicked twice, event handler responsible for likes is called twice', () => {
+      const viewButton = component.getByText('View')
+      fireEvent.click(viewButton)
+
+      const likeButton = component.getByText('Like')
+      fireEvent.click(likeButton)
+      fireEvent.click(likeButton)
+
+      expect(likeBlog.mock.calls.length).toBe(2)
+    })
+  })
 })
