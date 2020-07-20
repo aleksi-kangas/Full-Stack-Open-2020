36130725574bb0d4c2f52f87afb7d8f7d65a516f
Aleksi Kangas <aokang@utu.fi>
2020-07-20 14:55:36 +0300

    Part 6.21

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,3 +1,5 @@
+let timeoutID = undefined
+
 const notificationReducer = (state = null, action) => {
   switch (action.type) {
     case 'SET_NOTIFICATION':
@@ -10,15 +12,17 @@ const notificationReducer = (state = null, action) => {
 }
 
 export const setNotification = (notification, timeInSeconds) => {
+  if (timeoutID) {
+    clearTimeout(timeoutID)
+  }
   return async dispatch => {
     dispatch({
       type: 'SET_NOTIFICATION',
       data: notification
     })
-    setTimeout(() => {
-      dispatch({
-        type: 'REMOVE_NOTIFICATION'
-      })
+    timeoutID = setTimeout(() => {
+      dispatch(removeNotification())
+      timeoutID = undefined
     }, timeInSeconds * 1000)
   }
 }
