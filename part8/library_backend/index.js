9675fefd2ec4afb4a95a569e97c6487ac33d4413
Aleksi Kangas <aokang@utu.fi>
2020-07-25 17:34:39 +0300

    Part 8.5

--- part8/library_backend/index.js
+++ part8/library_backend/index.js
@@ -87,7 +87,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book]!
+    allBooks(author: String, genre: String): [Book]!
     allAuthors: [Author]!
   }
   
@@ -112,10 +112,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+      if (!args.author && !args.genre) {
         return books
+      } else if (args.author) {
+        return books.filter(book => book.author === args.author)
+      } else {
+        return books.filter(book => book.genres.includes(args.genre))
       }
-      return books.filter(book => book.author === args.author)
     },
     allAuthors: () => authors
   },
